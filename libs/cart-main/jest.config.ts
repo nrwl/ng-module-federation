154287bc81efd13722f25da1cdde7b99556f54ab
Colum Ferry <cferry09@gmail.com>
2022-06-06 11:56:04 +0100

    chore(repo): migrate repo to nx 14.2.0-rc.2 (#9)

--- libs/cart-main/jest.config.ts
+++ libs/cart-main/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   displayName: 'cart-main',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
--- libs/shop-main/jest.config.ts
+++ libs/shop-main/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   displayName: 'shop-main',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
